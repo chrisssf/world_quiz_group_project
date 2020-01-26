72f05d8485077147581ac631bd3ac65ab5f381fc
Steve Vance <steve.vance@hotmail.co.uk>
2020-01-26 12:21:02 +0000

    Stuck with terminal error

--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-const bodyParser = require( 'body-parser' );
+const cors = require('cors');
 
-const cors = require( 'cors' );
+app.use(cors())
+app.use(bodyParser.json());
 
-app.use( cors() )
-app.use( bodyParser.json() );
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
 
-const MongoClient = require( 'mongodb' ).MongoClient;
-const createRouter = require( './helpers/create_router.js' );
-
-MongoClient.connect( 'mongodb://localhost:27017' )
-  .then(( client ) => {
-    const db = client.db( 'quiz_questions' );
-    const countriesCollection = db.collection( 'countries' );
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('quiz_questions');
+    const countriesCollection = db.collection('countries');
   //need to add in second collection 'capitals'
-  app.use( 'api/countries', createRouter( countriesCollection ));
+  app.use( '/api/countries', createRouter( countriesCollection ));
   //need to add in second url and createRouter 'capitals'
 })
-
 .catch( console.error );
 
 app.listen( 3000, function() {
