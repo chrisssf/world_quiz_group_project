7791d25d1c81af341b137e2662b1519cdcec943d
Cody Abbott <cody.abbott16@gmail.com>
2020-01-27 14:30:50 +0000

    refactored capitals seed data

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -4,7 +4,7 @@ db.dropDatabase();
 db.countries.insertMany([
 
   {
-    "Question": "Which one of the countries above is France",
+    "Question": "Which one of the countries above is France?",
     "Options": [[{v: "India", f: "?"}], [{v: "Ukraine", f: "?"}], [{v: "France", f: "?"}], [{v: "Algeria", f: "?"}]],
     "Answer": "France"
     //Should the answer be a string or the index of the array?
@@ -39,31 +39,31 @@ db.countries.insertMany([
 db.capitals.insertMany([
   {
     "Question": "Which country's capital city is Madrid?",
-    "Options": ['India', 'Spain', 'France', 'Algeria'],
+    "Options": [[{v: "India", f: "?"}], [{v: "Spain", f: "?"}], [{v: "France", f: "?"}], [{v: "Algeria", f: "?"}]],
     "Answer": "Spain"
     //Should the answer be a string or the index of the array?
   },
   {
     "Question": "Which country's capital city is Rio de Janeiro?",
-    "Options": ["Mexico", "Canada", "Argentina", "Brazil"],
+    "Options": [[{v: "Mexico", f: "?"}], [{v: "Canada", f: "?"}], [{v: "Argentina", f: "?"}], [{v: "Brazil", f: "?"}]],
     "Answer": "Brazil"
     //Should the answer be a string or the index of the array?
   },
   {
     "Question": "Which country's capital city is Canberra?",
-    "Options": ["India", "Australia", "Mongolia", "China"],
+    "Options": [[{v: "India", f: "?"}], [{v: "Australia", f: "?"}], [{v: "Mongolia", f: "?"}], [{v: "China", f: "?"}]],
     "Answer": "Australia"
     //Should the answer be a string or the index of the array?
   },
   {
     "Question": "Which country's capital city is Berlin?",
-    "Options": ["Canada", "USA", "Alaska", "Germany"],
+    "Options": [[{v: "Canada", f: "?"}], [{v: "United States", f: "?"}], [{v: "Egypt", f: "?"}], [{v: "Germany", f: "?"}]],
     "Answer": "Germany"
     //Should the answer be a string or the index of the array?
   },
   {
     "Question": "Which country's capital city is Stockholm?",
-    "Options": ["Saudi Arabia", "Ukraine", "Italy", "Sweden"],
+    "Options": [[{v: "Saudi Arabia", f: "?"}], [{v: "Ukraine", f: "?"}], [{v: "Italy", f: "?"}], [{v: "Sweden", f: "?"}]],
     "Answer": "Sweden"
     //Should the answer be a string or the index of the array?
   }
