51ee567ba8fc639f33cf14944deb32a1d2a0b150
Cody Abbott <cody.abbott16@gmail.com>
2020-01-27 12:25:47 +0000

    title css update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
     <div class="grid-container">
-      <h1 class="title">World Map</h1>
+      <h1 class="title">
+
+          <span id="W">W</span>
+          <span id="O">O</span>
+          <span id="R">R</span>
+          <span id="L">L</span>
+          <span id="D">D</span>
+
+          <span id="M">M</span>
+          <span id="A">A</span>
+          <span id="P">P</span>
+
+      </h1>
       <map-view class="map" v-if="this.loaded" :countriesForMap='countriesForMap' :key="componentKey"/>
       <country-info class="map-info-box" v-if='mapCountryInfo !== null && answer === null' :mapCountryInfo='mapCountryInfo' />
       <selection-buttons class="quiz-choices" />
@@ -84,7 +96,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
   margin-top: 60px;
 }
 
@@ -98,5 +110,43 @@ export default {
   bottom: -105px;
 }
 
+.title {
+  font-size: 3em;
+}
+
+#W{
+  color: #57cfff;
+  text-shadow: 1px 0 0;
+}
+
+#O{
+color: #ff2921;
+}
+
+#R{
+color: #fff200;
+}
+
+#L{
+color: #51ff1c;
+}
+
+#D{
+color: #57cfff;
+margin-right: 10px;
+}
+
+#M{
+color: #ff2921;
+margin-left: 10px;
+}
+
+#A{
+color: #fff200;
+}
+
+#P{
+color: #51ff1c;
+}
 
 </style>
