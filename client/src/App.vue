937c8908215a745770faebb0de7f2d5ca3e1e4df
Cody Abbott <cody.abbott16@gmail.com>
2020-01-28 15:34:07 +0000

    changed background colour

--- client/src/App.vue
+++ client/src/App.vue
@@ -153,13 +153,17 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #F8F8ff;
+  color: #303030;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* background-color: #F5F5F5; */
   /* color: #2c3e50; */
-  margin-top: 20px;
+
 }
 
 .grid-container {
